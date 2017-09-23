5d59ebc0ab4b577ae40f79d2293561d02a980d04
Andrei Boltachev <andrewboltachev@gmail.com>
2017-09-23 18:38:13 +0300

    fix

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cycle/lein-template "0.1.0"
+(defproject cycledd/lein-template "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
--- src/leiningen/new/cycledd.clj
+++ src/leiningen/new/cycledd.clj
@@ -11,10 +11,9 @@
               :sanitized (name-to-path name)}]
     (main/info "Generating fresh 'lein new' cycledd project.")
     (->files data
-             [
              ["src/{{sanitized}}/core.clj" (render "core.clj" data)]
              ["src/{{sanitized}}/utils.clj" (render "utils.clj" data)]
              ["src/{{sanitized}}/stuff.clj" (render "stuff.clj" data)]
              ["project.clj" (render "project.clj" data)]
-             [".gitignore" (render "gitignore" data)]]
+             [".gitignore" (render "gitignore" data)]
              )))
